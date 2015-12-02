1410fc5e7e2e38f2843a72a1004ab4a25786ec6f
Willem Thiart <himself@willemthiart.com>
2015-12-02 20:05:47 +0700

    Add raft_node_is_voting

--- src/raft_node.c
+++ src/raft_node.c
@@ -16,6 +16,9 @@
 
 #include "raft.h"
 
+#define RAFT_NODE_VOTED_FOR_ME 1
+#define RAFT_NODE_VOTING 1 << 1
+
 typedef struct
 {
     void* udata;
@@ -23,8 +26,7 @@ typedef struct
     int next_idx;
     int match_idx;
 
-    /* 1 if node has voted for me */
-    int voted_for_me;
+    int flags;
 
     int id;
 } raft_node_private_t;
@@ -37,6 +39,7 @@ raft_node_t* raft_node_new(void* udata, int id)
     me->next_idx = 1;
     me->match_idx = 0;
     me->id = id;
+    me->flags = RAFT_NODE_VOTING;
     return (raft_node_t*)me;
 }
 
@@ -80,13 +83,32 @@ void raft_node_set_udata(raft_node_t* me_, void* udata)
 void raft_node_vote_for_me(raft_node_t* me_, const int vote)
 {
     raft_node_private_t* me = (raft_node_private_t*)me_;
-    me->voted_for_me = vote;
+    if (vote)
+        me->flags |= RAFT_NODE_VOTED_FOR_ME;
+    else
+        me->flags &= ~RAFT_NODE_VOTED_FOR_ME;
 }
 
 int raft_node_has_vote_for_me(raft_node_t* me_)
 {
     raft_node_private_t* me = (raft_node_private_t*)me_;
-    return me->voted_for_me;
+    return (me->flags & RAFT_NODE_VOTED_FOR_ME) != 0;
+}
+
+void raft_node_set_voting(raft_node_t* me_, int voting)
+{
+    raft_node_private_t* me = (raft_node_private_t*)me_;
+    if (voting)
+        me->flags |= RAFT_NODE_VOTING;
+    else
+        me->flags &= ~RAFT_NODE_VOTING;
+
+}
+
+int raft_node_is_voting(raft_node_t* me_)
+{
+    raft_node_private_t* me = (raft_node_private_t*)me_;
+    return (me->flags & RAFT_NODE_VOTING) != 0;
 }
 
 int raft_node_get_id(raft_node_t* me_)
