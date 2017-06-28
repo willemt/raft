6007088a799291a6bca4db5a1938000d9cfbeca6
Sergey Skorokhod <skorokhod.sergey@gmail.com>
2017-06-29 02:08:22 +0300

    null pointer checks added

--- src/raft_server.c
+++ src/raft_server.c
@@ -176,6 +176,7 @@ int raft_periodic(raft_server_t* me_, int msec_since_last_period)
 
     /* Only one voting node means it's safe for us to become the leader */
     if (1 == raft_get_num_voting_nodes(me_) &&
+        raft_get_my_node(me_) &&
         raft_node_is_voting(raft_get_my_node((void*)me)) &&
         !raft_is_leader(me_))
         raft_become_leader(me_);
@@ -188,6 +189,7 @@ int raft_periodic(raft_server_t* me_, int msec_since_last_period)
     else if (me->election_timeout <= me->timeout_elapsed)
     {
         if (1 < raft_get_num_voting_nodes(me_) &&
+            raft_get_my_node(me_) &&
             raft_node_is_voting(raft_get_my_node(me_)))
             raft_election_start(me_);
     }
