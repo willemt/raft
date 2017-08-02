290483d8ae3d888cc7912aa4225cfc5f7862a977
Anuj Kalia <akalia@cs.cmu.edu>
2017-08-02 12:21:55 -0600

    Fix install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-mkdir -p /usr/local/include/raft
+sudo mkdir -p /usr/local/include/raft
 sudo cp $(dirname $0)/include/raft.h /usr/local/include/raft/
 sudo cp $(dirname $0)/libcraft.* /usr/local/lib
 sudo ldconfig
